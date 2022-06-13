379533b6637b2a4868a379820e5196d517e691f6
Zhurbin <zhurbin.alexander@gmail.com>
2022-06-13 15:31:09 +0300

    Added single quote escaping(for FR lang)

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ const YAML = require('./json-to-yaml.js');
 
 const csvFilePath='./input/translate.csv';
 
+// Escape single "'" characters (for French language for example)
+const escapeCharacters = escapedString => {
+  const lettersArr = [...escapedString];
+
+
+  const escapedLettersArr = lettersArr.reduce((acc, item, index) => {
+    if (item === "'" && _.get(lettersArr, [index + 1]) !== "'" && _.get(lettersArr, [index - 1]) !== "'") {
+      return [...acc, item, "'"];
+    }
+    
+    return [...acc, item];
+  }, []);
+
+  const newEscapedString = escapedLettersArr.join("");
+
+  return newEscapedString;
+}
+
 csv({
   noheader: true,
   delimiter: ';'
 })
 .fromFile(csvFilePath)
 .then((jsonArray)=>{
-  const newJson = jsonArray.reduce((acc, item) => {
+  const newJson = jsonArray.reduce((acc, item, index) => {
     const string = item.field1.replace(/\//g, ".");
 
+    if (index === 18) {
+      console.log({ item });
+    }
     // Used setWith instead set, for case then numbers is a keys for object
-    const newAcc = _.setWith(acc, string, item.field2, Object);
+    const newAcc = _.setWith(acc, string, escapeCharacters(item.field2), Object);
 
     return {
       ...newAcc,
